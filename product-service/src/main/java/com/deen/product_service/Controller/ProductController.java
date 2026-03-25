f80c74d6cd6bd7924b5597e53401393905a065d5
dayanand15 <dayanand1509@gmail.com>
2026-03-25 23:50:14 +0530

    setup of user-service and product-service with config,eureka

---
+++ product-service/src/main/java/com/deen/product_service/Controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.deen.product_service.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+  
+   @GetMapping("/test")
+  public String test(){
+    return "Product service working";
+  }
+}
