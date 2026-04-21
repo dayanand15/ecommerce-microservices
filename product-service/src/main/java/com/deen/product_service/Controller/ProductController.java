d2a36ac660ad9183a55e78c266133213d6f9dc91
dayanand15 <dayanand1509@gmail.com>
2026-04-21 20:31:34 +0530

    Added Standerdized Api response in both user-service and product-service

--- product-service/src/main/java/com/deen/product_service/Controller/ProductController.java
+++ product-service/src/main/java/com/deen/product_service/Controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.deen.product_service.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.deen.product_service.dto.ApiResponse;
 import com.deen.product_service.dto.ProductRequest;
 import com.deen.product_service.dto.ProductResponse;
 import com.deen.product_service.service.ProductService;
@@ -26,28 +28,63 @@ public class ProductController {
   private final ProductService productService;
   
   @PostMapping("/create")
-  public ProductResponse createProduct(@RequestBody @Valid ProductRequest productRequest){
-    return productService.createProduct(productRequest);
+  public ApiResponse<ProductResponse> createProduct(@RequestBody @Valid ProductRequest productRequest){
+    ProductResponse productResponse = productService.createProduct(productRequest);
+
+    return new ApiResponse<>(
+      LocalDateTime.now(),
+      201, 
+      "Product created successfully",
+      productResponse
+    );
   }
 
   @GetMapping()
-  public List<ProductResponse> getAllProducts(){
-    return productService.getAllProducts();
+  public ApiResponse<List<ProductResponse>> getAllProducts(){
+    List<ProductResponse> productResponse= productService.getAllProducts();
+
+    return new ApiResponse<List<ProductResponse>>
+    ( LocalDateTime.now(),
+      200, 
+      "Product fetched successfully",
+      productResponse
+    );
   }
 
   @GetMapping("/{product_id}")
-  public ProductResponse getProduct(@PathVariable Long product_id){
-    return productService.getProductById(product_id);
+  public ApiResponse<ProductResponse> getProduct(@PathVariable Long product_id){
+    ProductResponse productResponse= productService.getProductById(product_id);
+
+     return new ApiResponse<ProductResponse>
+    ( LocalDateTime.now(),
+      200, 
+      "Product fetched successfully",
+      productResponse
+    ); 
   }
 
   @PutMapping("/{product_id}")
-  public ProductResponse updateProduct(@PathVariable Long product_id,@Valid @RequestBody ProductRequest productRequest){
-    return productService.updateProduct(product_id, productRequest);
+  public ApiResponse<ProductResponse> updateProduct(@PathVariable Long product_id,@Valid @RequestBody ProductRequest productRequest){
+    ProductResponse productResponse= productService.updateProduct(product_id, productRequest);
+
+     return new ApiResponse<ProductResponse>
+    ( LocalDateTime.now(),
+      200, 
+      "Product updated successfully",
+      productResponse
+    );
   }
 
   @DeleteMapping("/{product_id}")
-    public void deleteProduct(@PathVariable Long product_id){
-       productService.deleteProduct(product_id);
+    public ApiResponse<String> deleteProduct(@PathVariable Long product_id){
+        productService.deleteProduct(product_id);
+
+        return new ApiResponse<String> 
+    ( LocalDateTime.now(),
+      200, 
+      "Product deleted successfully",
+      "Product deleted successfully"
+    );
     }
 
 }
--- user-service/src/main/java/com/deen/user_service/Controller/UserController.java
+++ user-service/src/main/java/com/deen/user_service/Controller/UserController.java
@@ -1,5 +1,6 @@
 package com.deen.user_service.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.deen.user_service.dto.ApiResponse;
 import com.deen.user_service.dto.UserRequest;
 import com.deen.user_service.dto.UserResponse;
 import com.deen.user_service.service.UserService;
@@ -26,27 +28,62 @@ public class UserController {
   private final UserService userService;
 
   @PostMapping("/create")
-  public UserResponse createUser(@RequestBody @Valid UserRequest userRequest){
-    return userService.createUser(userRequest);
+  public ApiResponse<UserResponse> createUser(@RequestBody @Valid UserRequest userRequest){
+     UserResponse userResponse=userService.createUser(userRequest);
+
+     return new ApiResponse<UserResponse>
+     (LocalDateTime.now(),
+      201,
+      "User created successfully",
+      userResponse
+    );
   }
 
   @GetMapping("/{user_id}")
-  public UserResponse getUser(@PathVariable Long user_id){
-    return userService.getUserById(user_id);
+  public ApiResponse<UserResponse> getUser(@PathVariable Long user_id){
+    UserResponse userResponse= userService.getUserById(user_id);
+
+    return new ApiResponse<UserResponse>
+     (LocalDateTime.now(),
+      200,
+      "User fetched successfully",
+      userResponse
+    );
   }
 
   @GetMapping()
-  public List<UserResponse> getAllUsers(){
-    return userService.getAllUsers();
+  public ApiResponse<List<UserResponse>> getAllUsers(){
+    List<UserResponse> userResponse= userService.getAllUsers();
+
+    return new ApiResponse<List<UserResponse>>
+     (LocalDateTime.now(),
+      200,
+      "User fetched successfully",
+      userResponse
+    );
   }
 
   @PutMapping("/{user_id}")
-  public UserResponse updateUser(@PathVariable Long user_id,@Valid @RequestBody UserRequest userRequest){
-    return userService.updateUser(user_id,userRequest);
+  public ApiResponse<UserResponse> updateUser(@PathVariable Long user_id,@Valid @RequestBody UserRequest userRequest){
+    UserResponse userResponse= userService.updateUser(user_id,userRequest);
+
+     return new ApiResponse<UserResponse>
+     (LocalDateTime.now(),
+      200,
+      "User updated successfully",
+      userResponse
+    );
   }
 
   @DeleteMapping("{user_id}")
-  public void deleteUser(@PathVariable Long user_id){
+  public ApiResponse<String> deleteUser(@PathVariable Long user_id){
     userService.deleteUser(user_id);
+
+     return new ApiResponse<String>
+     (LocalDateTime.now(),
+      200,
+      "User deleted successfully",
+      "User deleted successfully"
+    );
   }
 }
