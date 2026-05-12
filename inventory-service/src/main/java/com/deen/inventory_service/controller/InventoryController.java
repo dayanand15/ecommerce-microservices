c7829f22547a5e576bb984ea831e45158cdaed5b
dayanand15 <dayanand1509@gmail.com>
2026-05-12 21:52:03 +0530

    Added Resilience (Circuit breaker) and added ResilienceService.

--- inventory-service/src/main/java/com/deen/inventory_service/controller/InventoryController.java
+++ inventory-service/src/main/java/com/deen/inventory_service/controller/InventoryController.java
@@ -1,12 +1,11 @@
 package com.deen.inventory_service.controller;
 
-import com.deen.inventory_service.dto.ApiResponse;
-import com.deen.inventory_service.dto.InventoryResponse;
+
 import com.deen.inventory_service.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
+
 
 @RestController
 @RequestMapping("/inventory")
